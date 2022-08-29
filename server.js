9e685669284326352b1d7e8f83dd04cf97289182
Developer <nextbackseo@gmail.com>
2022-08-29 14:20:11 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const serverStatic = require('serve-static')
+const path = require('path')
+const app = express()
+
+app.use('/', serverStatic(path.join(__dirname,'/dist')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'./dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
+console.log('app is on PORT :'+ port);
